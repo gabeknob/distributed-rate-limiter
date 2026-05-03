6d4f92ab715d720cf76a7d1adc2850328632b13b
Gabriel Nobrega <gabrielnobrega63@gmail.com>
2026-05-03 15:07:23 -0300

    feat: Adding network stack and piping

--- infra/cmd/infra/main.go
+++ infra/cmd/infra/main.go
@@ -1,8 +1,27 @@
 package main
 
 import (
+	"os"
+
+	stack "github.com/gabeknob/distributed-rate-limiter/infra/internal/stack"
+
+	"github.com/aws/aws-cdk-go/awscdk/v2"
+	"github.com/aws/jsii-runtime-go"
 )
 
 func main() {
-	
+	defer jsii.Close()
+
+	app := awscdk.NewApp(nil)
+
+	env := &awscdk.Environment{
+		Account: jsii.String(os.Getenv("CDK_DEFAULT_ACCOUNT")),
+		Region:  jsii.String(os.Getenv("CDK_DEFAULT_REGION")),
+	}
+
+	defaultProps := &awscdk.StackProps{Env: env}
+
+	stack.NewNetworkStack(app, "NetworkStack", defaultProps)
+
+	app.Synth(nil)
 }
---
+++ infra/internal/stack/network_stack.go
@@ -0,0 +1,43 @@
+package stack
+
+import (
+	"github.com/aws/aws-cdk-go/awscdk/v2"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsec2"
+	"github.com/aws/constructs-go/constructs/v10"
+	"github.com/aws/jsii-runtime-go"
+)
+
+type NetworkStack struct {
+	Stack awscdk.Stack
+	Vpc awsec2.IVpc
+}
+
+func NewNetworkStack(scope constructs.Construct, id string, props *awscdk.StackProps) *NetworkStack {
+	stack := awscdk.NewStack(scope, &id, props)
+
+	vpc := awsec2.NewVpc(stack, jsii.String("Rate-Limiter-VPC"), &awsec2.VpcProps{
+		IpAddresses: awsec2.IpAddresses_Cidr(jsii.String("10.0.0.0/16")),
+		CreateInternetGateway: jsii.Bool(true),
+		EnableDnsHostnames: jsii.Bool(true),
+		EnableDnsSupport: jsii.Bool(true),
+		MaxAzs: jsii.Number(2),
+		NatGateways: jsii.Number(1),
+		SubnetConfiguration: &[]*awsec2.SubnetConfiguration{
+			{
+				Name: jsii.String("Rate-Limiter-Public"),
+				CidrMask: jsii.Number(24),
+				SubnetType: awsec2.SubnetType_PUBLIC,
+			},
+			{
+				Name: jsii.String("Rate-Limiter-Private"),
+				CidrMask: jsii.Number(24),
+				SubnetType: awsec2.SubnetType_PRIVATE_WITH_EGRESS,
+			},
+		},
+	})
+
+	return &NetworkStack{
+		Stack: stack,
+		Vpc:   vpc,
+	}
+}
